ec83b65f744476efc19de6ca75a93821072af70a
Konstantin <the.kovet24@gmail.com>
2026-01-31 10:01:44 +0300

    First commit. Create packages

---
+++ src/main/java/salon/ekat/hairStylist/HairStylistApplication.java
@@ -0,0 +1,13 @@
+package salon.ekat.hairStylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairStylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairStylistApplication.class, args);
+	}
+
+}
